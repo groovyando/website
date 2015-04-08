e6077996f0d639ac6bd60fb601406c309d2c36a6
Domingo Suarez Torres <domingo.suarez@gmail.com>
2015-04-07 23:45:25 -0500

    fixed baseUrl, fixed remote deploy directory and added 'fork me' github ribbon

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rm -rf public && hugo --theme=default --verbose && rsync -avzhe ssh --delete --delete-after ./public deployer@198.58.120.79:/www/hackergarten.mx
+rm -rf public && hugo --theme=default --verbose && rsync -avzhe ssh --delete --delete-after ./public deployer@198.58.120.79:/www/grails.org.mx
