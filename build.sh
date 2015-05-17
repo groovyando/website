b145d3ede238ff7228fac089770170d9c39d321d
Domingo Suarez Torres <domingo.suarez@gmail.com>
2015-05-17 12:00:11 -0500

    removed broken commands

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-rm -rf software
-mkdir software
-rm -rf public && hugo --verbose && mv public/* software && mv software public/ && cp foo/* public
+rm -rf public && hugo --verbose
